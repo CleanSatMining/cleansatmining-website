82cbbe3e88e7b91ee1066b544e95057d04991a1b
Cyrille <cdurbal@cleansatmining.com>
2024-07-04 12:12:33 +0200

    fix: footer

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -1,208 +1,140 @@
+"use client";
+import { NavLink } from "@/models/NavLink";
+import { DiscordLogo } from "@phosphor-icons/react/dist/ssr/DiscordLogo";
+import { LinkedinLogo } from "@phosphor-icons/react/dist/ssr/LinkedinLogo";
+import { TwitterLogo } from "@phosphor-icons/react/dist/ssr/TwitterLogo";
+import { YoutubeLogo } from "@phosphor-icons/react/dist/ssr/YoutubeLogo";
+import classNames from "classnames";
+import Image from "next/image";
 import Link from "next/link";
-import Logo from "./logo";
+import { usePathname } from "next/navigation";
+import { useMemo } from "react";
+
+export const footerLinks: NavLink[] = [
+  {
+    href: "mailto:contact@cleansatmining.com ",
+    label: "contact",
+    external: true,
+  },
+  {
+    href: "https://cleansatmining.com/legal-notices ",
+    label: "Mentions légales",
+    external: true,
+  },
+  {
+    href: "https://cleansatmining.com/privacy-policy ",
+    label: "Confidentialité",
+    external: true,
+  },
+];
+
+export const socialLinks: NavLink[] = [
+  {
+    href: process.env.NEXT_PUBLIC_RS_TWITTER ?? "",
+    label: "twitter",
+    external: true,
+    icon: TwitterLogo,
+  },
+  {
+    href: process.env.NEXT_PUBLIC_RS_YOUTUBE ?? "",
+    label: "youtube",
+    external: true,
+    icon: YoutubeLogo,
+  },
+  {
+    href: process.env.NEXT_PUBLIC_RS_LINKEDIN ?? "",
+    label: "linkedin",
+    external: true,
+    icon: LinkedinLogo,
+  },
+  {
+    href: process.env.NEXT_PUBLIC_RS_DISCORD ?? "",
+    label: "discord",
+    external: true,
+    icon: DiscordLogo,
+  },
+];
 
 export default function Footer() {
+  const pathname = usePathname();
+
+  const footerLinksElements = useMemo(
+    () =>
+      footerLinks.map((link, index) => {
+        const isActiveLink = !link.external && pathname === link.href;
+        return (
+          <li
+            key={link.label}
+            className={`lg:px-3 ${
+              index !== 0 ? "lg:border-l-[1px] lg:border-green" : ""
+            }`}
+          >
+            <Link
+              href={link.href}
+              className={classNames(
+                "border-b-[1px] py-1",
+                isActiveLink ? "border-green" : "border-transparent",
+                "font-semibold leading-5 hover:text-green"
+              )}
+              target={link.external ? "_blank" : undefined}
+              rel={link.external ? "noopener noreferrer" : undefined}
+              title={
+                link.external ? `${link.label} - ${"externalLink"}` : undefined
+              }
+            >
+              {link.label}
+            </Link>
+          </li>
+        );
+      }),
+    [pathname]
+  );
+
+  const socialLinksElements = useMemo(
+    () =>
+      socialLinks.map((link) => {
+        if (link.href && link.href !== "")
+          return (
+            <li key={link.label}>
+              <Link
+                href={link.href}
+                className="inline-flex h-10 w-10 items-center justify-center rounded-full bg-green p-2 leading-5 hover:bg-green/90 dark:text-grey-800"
+                target={link.external ? "_blank" : ""}
+                rel={link.external ? "noopener noreferrer" : ""}
+                title={
+                  link.external
+                    ? `${link.label} - ${"externalLink"}`
+                    : undefined
+                }
+              >
+                <span className={classNames(link.icon ? "sr-only" : "")}>
+                  {link.label}
+                </span>
+                {link.icon && <link.icon size={24} role="img" />}
+              </Link>
+            </li>
+          );
+      }),
+    []
+  );
+
   return (
-    <footer>
-      <div className="max-w-6xl mx-auto px-4 sm:px-6">
-        {/* Blocks */}
-        <div className="grid sm:grid-cols-12 gap-8 py-8 md:py-12">
-          {/* 1st block */}
-          <div className="sm:col-span-12 lg:col-span-4 lg:max-w-xs">
-            <div className="mb-2">
-              {/* Logo */}
-              <Logo />
-              {/* <Link className="inline-flex" href="/" aria-label="Cruip">
-                <svg className="w-8 h-8" viewBox="0 0 32 32" xmlnsXlink="http://www.w3.org/1999/xlink">
-                  <defs>
-                    <radialGradient cx="50%" cy="89.845%" fx="50%" fy="89.845%" r="108.567%" gradientTransform="matrix(-.00915 -.82755 .99996 -.00757 -.394 1.319)" id="logo2-b">
-                      <stop stopColor="#3B82F6" stopOpacity=".64" offset="0%" />
-                      <stop stopColor="#F472B6" stopOpacity=".876" offset="100%" />
-                    </radialGradient>
-                    <radialGradient cx="50%" cy="89.845%" fx="50%" fy="89.845%" r="108.567%" gradientTransform="matrix(-.00915 -.82755 .99996 -.00757 -.394 1.319)" id="logo2-d">
-                      <stop stopColor="#3B82F6" stopOpacity=".64" offset="0%" />
-                      <stop stopColor="#D375C2" stopOpacity=".833" offset="50.358%" />
-                      <stop stopColor="#FBCFE8" stopOpacity=".876" offset="100%" />
-                    </radialGradient>
-                    <path d="M12 32c8-6.915 12-12.582 12-17 0-6.627-5.373-12-12-12S0 8.373 0 15c0 4.418 4 10.085 12 17Z" id="logo2-a" />
-                    <path d="M20 29c8-6.915 12-12.582 12-17 0-6.627-5.373-12-12-12S8 5.373 8 12c0 4.418 4 10.085 12 17Z" id="logo2-c" />
-                  </defs>
-                  <g fill="none" fillRule="evenodd">
-                    <use fill="url(#logo2-b)" opacity=".64" transform="matrix(1 0 0 -1 0 35)" xlinkHref="#logo2-a" />
-                    <use fill="url(#logo2-d)" opacity=".961" xlinkHref="#logo2-c" />
-                  </g>
-                </svg>
-              </Link> */}
-            </div>
-            <div className="text-sm text-gray-600">
-              <a
-                className="text-gray-400 hover:text-blue-500 transition duration-150 ease-in-out"
-                href="#0"
-              >
-                Terms
-              </a>{" "}
-              ·{" "}
-              <a
-                className="text-gray-400 hover:text-blue-500 transition duration-150 ease-in-out"
-                href="#0"
-              >
-                Privacy Policy
-              </a>
-            </div>
-          </div>
-          {/* 2nd block */}
-          <div className="sm:col-span-6 md:col-span-3 lg:col-span-2">
-            <h6 className="text-xs text-gray-200 font-semibold uppercase mb-2">
-              Products
-            </h6>
-            <ul className="text-sm space-y-2">
-              <li>
-                <a
-                  className="text-gray-400 hover:text-blue-500 transition duration-150 ease-in-out"
-                  href="#0"
-                >
-                  Neon Manage
-                </a>
-              </li>
-              <li>
-                <a
-                  className="text-gray-400 hover:text-blue-500 transition duration-150 ease-in-out"
-                  href="#0"
-                >
-                  Neon Analyse
-                </a>
-              </li>
-              <li>
-                <a
-                  className="text-gray-400 hover:text-blue-500 transition duration-150 ease-in-out"
-                  href="#0"
-                >
-                  Neon Launch
-                </a>
-              </li>
-              <li>
-                <a
-                  className="text-gray-400 hover:text-blue-500 transition duration-150 ease-in-out"
-                  href="#0"
-                >
-                  Experimentation
-                </a>
-              </li>
-            </ul>
-          </div>
-          {/* 3rd block */}
-          <div className="sm:col-span-6 md:col-span-3 lg:col-span-2">
-            <h6 className="text-xs text-gray-200 font-semibold uppercase mb-2">
-              Resources
-            </h6>
-            <ul className="text-sm space-y-2">
-              <li>
-                <a
-                  className="text-gray-400 hover:text-blue-500 transition duration-150 ease-in-out"
-                  href="#0"
-                >
-                  Blog
-                </a>
-              </li>
-              <li>
-                <a
-                  className="text-gray-400 hover:text-blue-500 transition duration-150 ease-in-out"
-                  href="#0"
-                >
-                  Cheat Sheet
-                </a>
-              </li>
-              <li>
-                <a
-                  className="text-gray-400 hover:text-blue-500 transition duration-150 ease-in-out"
-                  href="#0"
-                >
-                  Channel Partners
-                </a>
-              </li>
-              <li>
-                <a
-                  className="text-gray-400 hover:text-blue-500 transition duration-150 ease-in-out"
-                  href="#0"
-                >
-                  Affiliate Program
-                </a>
-              </li>
-            </ul>
-          </div>
-          {/* 4th block */}
-          <div className="sm:col-span-6 md:col-span-3 lg:col-span-2">
-            <h6 className="text-xs text-gray-200 font-semibold uppercase mb-2">
-              Compare
-            </h6>
-            <ul className="text-sm space-y-2">
-              <li>
-                <a
-                  className="text-gray-400 hover:text-blue-500 transition duration-150 ease-in-out"
-                  href="#0"
-                >
-                  Session Recording
-                </a>
-              </li>
-              <li>
-                <a
-                  className="text-gray-400 hover:text-blue-500 transition duration-150 ease-in-out"
-                  href="#0"
-                >
-                  Feature Flags
-                </a>
-              </li>
-              <li>
-                <a
-                  className="text-gray-400 hover:text-blue-500 transition duration-150 ease-in-out"
-                  href="#0"
-                >
-                  Heatmaps
-                </a>
-              </li>
-              <li>
-                <a
-                  className="text-gray-400 hover:text-blue-500 transition duration-150 ease-in-out"
-                  href="#0"
-                >
-                  Correlation Analysis
-                </a>
-              </li>
-            </ul>
-          </div>
-          {/* 5th block */}
-          <div className="sm:col-span-6 md:col-span-3 lg:col-span-2">
-            <h6 className="text-xs text-gray-200 font-semibold uppercase mb-2">
-              Company
-            </h6>
-            <ul className="text-sm space-y-2">
-              <li>
-                <a
-                  className="text-gray-400 hover:text-blue-500 transition duration-150 ease-in-out"
-                  href="#0"
-                >
-                  About Us
-                </a>
-              </li>
-              <li>
-                <a
-                  className="text-gray-400 hover:text-blue-500 transition duration-150 ease-in-out"
-                  href="#0"
-                >
-                  Our Story
-                </a>
-              </li>
-              <li>
-                <a
-                  className="text-gray-400 hover:text-blue-500 transition duration-150 ease-in-out"
-                  href="#0"
-                >
-                  Work With Us
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
+    <footer className="flex flex-col gap-16 rounded-t-[48px] border-t-[1px] border-green/20 px-12 py-8">
+      <div className="flex flex-wrap items-center justify-between gap-8 lg:gap-4">
+        <Link href={"csmHome"}>
+          <Image src="/logo.svg" width={80} height={80} alt={"logo"} />
+        </Link>
+        <nav aria-label={"nav"}>
+          <ul className="flex flex-col flex-wrap gap-y-2 lg:flex-row lg:items-center lg:justify-center">
+            {footerLinksElements}
+          </ul>
+        </nav>
+        <ul className="flex items-center gap-6">{socialLinksElements}</ul>
+      </div>
+      <div className="flex flex-col items-center text-center dark:text-grey-300">
+        <p className="mb-3 text-sm font-semibold">
+          {"Copyright © 2024 CleanSat Mining SA"}
+        </p>
       </div>
     </footer>
   );
